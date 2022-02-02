9e69d2f84471528d06d7d85497a2fdf780dffee0
diegofdg <diegofdg@gmail.com>
2022-02-02 09:37:59 -0300

    recuperar password html

--- src/app/components/usuario/usuario-routing.module.ts
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 
 const routes: Routes = [
   {
     path: '', component: LoginComponent
+  },
+  {
+    path: 'recuperarPassword', component: RecuperarPasswordComponent
   }
 ];
 
