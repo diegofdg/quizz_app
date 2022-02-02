e1bd8eb7f7fa7fef28c9edd444e8765314747bc7
diegofdg <diegofdg@gmail.com>
2022-02-02 13:39:35 -0300

    register - validación personalizada

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +10,24 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-      usuario: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+    this.registerForm = this.fb.group({      
+    usuario: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],      
       repetirPassword: ['']
-    });
+    }, { validator: this.checkPassword } as AbstractControlOptions);   
   }
 
   ngOnInit(): void {
   }
 
   register() {
-    
+    console.log(this.registerForm);
+  }
+
+  checkPassword(group: FormGroup): any {
+    const pass = group.controls['password']?.value;
+    const confirmPassword = group.controls['repetirPassword']?.value;
+    return pass === confirmPassword ? null : { notSame: true }
   }
 
 }
