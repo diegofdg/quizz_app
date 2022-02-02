dbee5f5d1ca6163ea9d785861b51ebabfa7a46b8
diegofdg <diegofdg@gmail.com>
2022-02-02 00:22:22 -0300

    inicio.component validamos pin

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  error: boolean = false;
+  pin: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ingresar() {
+    if(this.pin === '') {
+      this.error = true;
+
+      setTimeout(() => {
+        this.error = false;
+      }, 3000);
+    }
+  }
 }
