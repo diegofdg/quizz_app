873f5b95722979a3fdcb9efc41f65340741b732d
diegofdg <diegofdg@gmail.com>
2022-02-03 10:32:43 -0300

    validamos campos

--- src/app/components/dashboard/crear-quizz/crear-quizz.component.ts
+++ src/app/components/dashboard/crear-quizz/crear-quizz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-quizz',
@@ -8,8 +9,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CrearQuizzComponent implements OnInit {
   cuestionarioForm: FormGroup;
+  mostrarError = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
     this.cuestionarioForm = this.fb.group({
       titulo: ['', Validators.required],
       descripcion: ['', Validators.required],
@@ -20,6 +25,11 @@ export class CrearQuizzComponent implements OnInit {
   }
 
   siguiente() {
-    console.log(this.cuestionarioForm);    
+    if(this.cuestionarioForm.invalid) {
+      this.mostrarError = true;
+      setTimeout(() => {
+        this.mostrarError = false;
+      }, 3000);
+    }
   }
 }
