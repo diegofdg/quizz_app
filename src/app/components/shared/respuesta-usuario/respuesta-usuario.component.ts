33b4b6a90d2aa658bb6a1b2ec26a238230b98353
diegofdg <diegofdg@gmail.com>
2022-02-06 07:40:58 -0300

    respuesta usuario component - obtenemos respuesta de firebase

--- src/app/components/shared/respuesta-usuario/respuesta-usuario.component.ts
+++ src/app/components/shared/respuesta-usuario/respuesta-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RespuestaQuizzService } from 'src/app/services/respuesta-quizz.service';
 
 @Component({
   selector: 'app-respuesta-usuario',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./respuesta-usuario.component.css']
 })
 export class RespuestaUsuarioComponent implements OnInit {
+  id: string;
+  loading = false;
 
-  constructor() { }
+  constructor(
+    private _respuestaUsuarioService: RespuestaQuizzService,
+    private aRoute: ActivatedRoute
+  ) {
+    this.id = this.aRoute.snapshot.paramMap.get('id')!;
+  }
 
   ngOnInit(): void {
+    this.obtenerRespuestaUsuario();
+  }
+
+  obtenerRespuestaUsuario() {
+    this.loading = true;
+    this._respuestaUsuarioService.getRespuestaUsuario(this.id).subscribe({
+      next: (responseOK) => {
+        console.log(responseOK.data());
+        this.loading = false;
+      },
+      error: (responseFail) => {
+        console.log(responseFail);
+        this.loading = false;
+      }
+    });
   }
 
 }
