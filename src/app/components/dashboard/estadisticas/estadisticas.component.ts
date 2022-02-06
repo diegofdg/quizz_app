6c05f3d74a45a4f9ecba13207acc2ed023223317
diegofdg <diegofdg@gmail.com>
2022-02-06 08:58:52 -0300

    delete respuestausuario

--- src/app/components/dashboard/estadisticas/estadisticas.component.ts
+++ src/app/components/dashboard/estadisticas/estadisticas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import { RespuestaQuizzService } from 'src/app/services/respuesta-quizz.service';
 
@@ -14,7 +15,11 @@ export class EstadisticasComponent implements OnInit, OnDestroy {
   listRespuestasUsuario: any[] = [];
   respuestasQuizz: Subscription = new Subscription();
 
-  constructor(private aRoute: ActivatedRoute, private _respuestaUsuarioService: RespuestaQuizzService) {
+  constructor(
+    private aRoute: ActivatedRoute, 
+    private _respuestaUsuarioService: RespuestaQuizzService,
+    private toastr: ToastrService
+  ) {
     this.id = this.aRoute.snapshot.paramMap.get('id')!;
   }
 
@@ -40,13 +45,24 @@ export class EstadisticasComponent implements OnInit, OnDestroy {
             ...element.payload.doc.data()
           })
         });
-        console.log(this.listRespuestasUsuario);
       },
       error: (responseFail) => {
         console.log(responseFail);
         this.loading = false;
       }
     });
+  }
+
+  eliminarRespuestaUsuario(id: string) {
+    this.loading = true;
+
+    this._respuestaUsuarioService.deleteRespuestaUsuario(id).then(() => {
+      this.loading = false;
+      this.toastr.info('La respuesta fue eliminada', 'Respuesta Eliminada')
+    }, error => {
+      console.log(error);
+      this.toastr.error('Opss.. ocurrio un error', 'Error');
+    })
   }
 
 }
